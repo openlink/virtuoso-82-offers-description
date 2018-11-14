e36475465ec0a8fdfe392fdfcfeb4f0941594e72
OpenLink Software <github@openlinksw.com>
2018-11-14 12:17:10 -0500

    Added other key graphs
    Completed target graph scoping

--- delete-expired-offers-from-shop-graph.sql
+++ delete-expired-offers-from-shop-graph.sql
@@ -1,6 +1,8 @@
 -- Cleanup
 -- Remove Expired Offers using SPASQL (SPARQL relations operated on via SQL)
 
+log_enable(1);
+
 SELECT * 
 FROM ( SPARQL
 WITH <urn:opl:shop:offering:sponging:cache:official> 
@@ -11,4 +13,28 @@ WHERE {
        FILTER (?valid < "2018-12-31T23:59:59Z"^^<http://www.w3.org/2001/XMLSchema#dateTime>
               ) 
      
-      } ) AS ObsoleteOffers FOR UPDATE
+      } ) AS ObsoleteOffers FOR UPDATE ;
+      
+SELECT * 
+FROM ( SPARQL
+WITH <urn:data:openlink:products>
+DELETE {?s ?p ?o}
+WHERE {
+       ?s a schema:Offer; schema:validThrough ?valid; ?p ?o . 
+       FILTER (isIRI(?s))
+       FILTER (?valid < "2018-12-31T23:59:59Z"^^<http://www.w3.org/2001/XMLSchema#dateTime>
+              ) 
+     
+} ) AS ObsoleteOffers FOR UPDATE ;
+
+SELECT * 
+FROM ( SPARQL
+WITH <http://my.openlinksw.com/dataspace/shop/0005/offerdata>
+DELETE {?s ?p ?o}
+WHERE {
+       ?s a schema:Offer; schema:validThrough ?valid; ?p ?o . 
+       FILTER (isIRI(?s))
+       FILTER (?valid < "2018-12-31T23:59:59Z"^^<http://www.w3.org/2001/XMLSchema#dateTime>
+              ) 
+     
+} ) AS ObsoleteOffers FOR UPDATE ;
